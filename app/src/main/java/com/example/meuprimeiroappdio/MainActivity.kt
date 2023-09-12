b5cb1ac5d07fe831d8546f91a9b28192b83e6bb5
Felipe Cunha <felipescunha21@gmail.com>
2023-09-12 18:41:24 -0300

    Commit de Update dos Idiomas

---
+++ app/src/main/java/com/example/meuprimeiroappdio/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.meuprimeiroappdio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // O desafo será criar uma string valor dentro do string.xml
+        // E trocar o texto do xml e tornar internacional (Inglês, Espanhol, etc...)
+    }
+}
